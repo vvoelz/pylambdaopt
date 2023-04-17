3a7a1a39140375aae5773ff8fbd2c9f155350c06
Vincent Voelz <vvoelz@gmail.com>
2023-04-17 16:58:46 -0400

    try this help text tweak

--- scripts/optimize_coul_vdw_lambdas.py
+++ scripts/optimize_coul_vdw_lambdas.py
@@ -273,6 +273,7 @@ if __name__ == '__main__':
               
     EXAMPLE
     Try this:
+        $ cd ../examples
         $ python ../scripts/optimize_coul_vdw_lambdas.py Sulfamethazine_tau0_0_water/ee.mdp Sulfamethazine_tau0_0_water/ee.dhdl.xvg opt Sulfamethazine_tau0_0_water
     """        
 
