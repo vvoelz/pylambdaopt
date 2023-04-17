0febf51652950bc71f97b6897b85a082f8faa0a7
Vincent Voelz <vvoelz@gmail.com>
2023-04-17 15:20:44 -0400

    copied from sampl9/logP

---
+++ scripts/ee_tools.py
@@ -0,0 +1,255 @@
+## ee_tools.py
+
+## Functions and classes to set up and analyze expanded ensemble (EE) simulations
+
+import os, sys
+import numpy as np
+
+import matplotlib.pyplot as plt
+#  %matplotlib inline
+
+import scipy 
+from scipy import stats	
+
+### Functions ###
+
+def get_fep_lambdas(mdpfile):
+    """Given an *.mdp file as input, extract the values of fep-lambdas
+    RETURNS
+    fep_lambdas      - numpy array of fep-lambdas
+    NOTE: for the alchemical transformations, the lambda=0 state is ligand L, 
+          and lambda=1 is ligand L*.
+    """
+    fin = open(mdpfile,'r')
+    lines = fin.readlines()
+    fin.close()
+   
+    fep_lambdas = None
+    for line in lines:
+        if line.count('fep_lambdas'):
+          fep_string = line.split('=')[1].strip() 
+          #print ('fep_string=', fep_string)
+          fep_lambdas = np.array([float(s) for s in fep_string.split()])
+          #print ('fep_lambdas=', fep_lambdas)
+
+    return fep_lambdas
+
+def get_coul_vdw_lambdas(mdpfile):
+    """Given an *.mdp file as input, extract the values of coul_lambdas and vdw_lambdas 
+
+    RETURNS
+    coul_lambdas      - numpy array of coul-lambdas
+    vdw_lambdas
+
+    """
+    fin = open(mdpfile,'r')
+    lines = fin.readlines()
+    fin.close()
+
+    coul_lambdas = None
+    for line in lines:
+        if (line.count('coul-lambdas') > 0) | (line.count('coul_lambdas') > 0):
+          coul_string = line.split('=')[1].strip()
+          #print ('coul_string=', coul_string)
+          coul_lambdas = np.array([float(s) for s in coul_string.split()])
+          #print ('coul_lambdas=', coul_lambdas)
+
+    vdw_lambdas = None
+    for line in lines:
+        if (line.count('vdw-lambdas') > 0) | (line.count('vdw_lambdas') > 0): 
+          vdw_string = line.split('=')[1].strip()
+          #print ('vdw_string=', vdw_string)
+          vdw_lambdas = np.array([float(s) for s in vdw_string.split()])
+          #print ('vdw_lambdas=', vdw_lambdas)
+
+    return coul_lambdas, vdw_lambdas
+
+
+def get_dhdl_data(dhdl_xvgfile, verbose=True):
+    r"""Read and parse the information in the dhdl file.
+    
+    RETURNS
+    time_in_ps      - time in ps (1D np.array)
+    thermo_states   - thermodynamic state indices (1D np.array)
+    dhdl            - delta_Uij (np.array of shape (N,K))
+                      where N is snapshots and K is number of thermodynamic states
+    pV              - a 1D np.array of the pV values, if simulation was NPT;
+                      None, otherwise
+                     
+    
+    **NOTE 1**:
+    For FEP vs couul/vdw the number of columns in the dhdl.xvg file will be different!
+    Here we look for the first appearance of "\xD\f{}H" ...
+
+    time         ---> Column 0 is time in ps.                         
+    thermo_index ---> Column 1 = @ s0 legend "Thermodynamic state"
+                      Column 2 = @ s1 legend "Total Energy (kJ/mol)"
+                      Column 3 = @ s2 legend "dH/d\xl\f{} fep-lambda = 0.0000"
+    
+    dU_ij starts ---> Column 4 = @ s3 legend "\xD\f{}H \xl\f{} to 0.0000"
+                      Column 5 = @ s4 legend "\xD\f{}H \xl\f{} to 0.0020"
+                      Column 6 = @ s5 legend "\xD\f{}H \xl\f{} to 0.0040"
+
+                         ...  
+                                 @ s104 legend "\xD\f{}H \xl\f{} to (0.0000, 1.0000, 1.0000, 1.0000)"
+                         --->    @ s105 legend "pV (kJ/mol)"
+
+    **NOTE 2**: if NPT, there will be an extra line for pV!
+
+
+    coul/vdw:
+
+                                 @ s0 legend "Thermodynamic state"
+                                 @ s1 legend "Total Energy (kJ/mol)"
+                                 @ s2 legend "dH/d\xl\f{} fep-lambda = 0.0000"
+                                 @ s3 legend "dH/d\xl\f{} coul-lambda = 0.0000"
+                                 @ s4 legend "dH/d\xl\f{} vdw-lambda = 0.0000"
+                                 @ s5 legend "dH/d\xl\f{} restraint-lambda = 1.0000"
+
+    """
+    
+    import os
+    assert os.path.exists(dhdl_xvgfile)
+    
+    # Read and parse the file
+    fin = open(dhdl_xvgfile,"r")
+    lines = fin.readlines()
+    print ('tot lines=', len(lines))
+    lines = lines[:2072]    #only using the first 2.ns data
+    print ('tot lines=', len(lines))
+    fin.close()
+    
+    # Search headers to find which column starts the dhdl data
+    dhdl_column_start = None
+    for line in lines:
+        # Looking for line like this: "@ s5 legend "\xD\f{}H \xl\f{} to (0.0000, 0.0000, 0.0000)"
+        if line.count('"\\xD\\f{}H') > 0:
+            dhdl_column_start = int(line.split(' ')[1].replace('s','')) + 1
+            break
+    if verbose:
+        print('dhdl data starts at column:', dhdl_column_start)
+
+    # Search headers to find pV, if it exists"
+    pV_column_start = None
+    for line in lines:
+        # Looking for line like this: 's105 legend "pV (kJ/mol)"'
+        if line.count('pV (kJ/mol') > 0:
+            pV_column_start = int(line.split(' ')[1].replace('s','')) + 1
+            break
+    if verbose:
+        if pV_column_start != None:
+            print('Found pV data at column:', pV_column_start)
+        else:
+            print('No pV data found.')
+
+
+
+    # Get rid of all the header lines
+    i = 0
+    while i < len(lines):
+        if (lines[i][0] == '#') or (lines[i][0] == '@'):
+            lines.pop(i)
+        else:
+            i+=1
+    
+    # find the correct number of entries from the first line
+    ncols = len(lines[0].strip().split())
+    print(lines[0])
+    print('ncols', ncols)
+    
+    time_in_ps, dhdl, thermo_states = [], [], []
+    pV_values = []
+    for line in lines:
+        line_data_list = [float(s) for s in line.strip().split()] 
+            
+        # Skip line if it doesn't have the correct number of entries
+        # (sometimes the I/O gets cutoff when writing the dhdl.xvg in it corrupts the data)
+        if len(line_data_list) == ncols:
+            time_in_ps.append(line_data_list[0])
+            thermo_states.append(line_data_list[1])
+            if pV_column_start == None:
+                dhdl.append(line_data_list[dhdl_column_start:])
+            else:
+                # ignore the last column if it contains pV data 
+                dhdl.append(line_data_list[dhdl_column_start:-1])
+                pV_values.append(line_data_list[-1])
+            
+    time_in_ps = np.array(time_in_ps)
+    dhdl = np.array(dhdl)
+    thermo_states = np.array(thermo_states)
+    
+    print('time_in_ps.shape', time_in_ps.shape)
+    print('dhdl.shape=', dhdl.shape)
+    print('thermo_states=', thermo_states)
+
+    if pV_column_start == None:
+        return time_in_ps, thermo_states, dhdl, None
+    else:
+        return time_in_ps, thermo_states, dhdl, np.array(pV_values) 
+
+
+
+
+def estimate_sigmas(dhdl, thermo_states, plot_data=True):
+    """Using as input the Delta_U_ij energies from the dhdl array, 
+    estimate the standard deviations P(U_{i-->i+1}) for neighboring ensembles.
+    
+    RETURNS
+    sigmas   - a np.array() of standard deviations P(U_{i-->i+1}).
+    """
+    
+    nlambdas = dhdl.shape[1]
+    print('nlambdas', nlambdas)
+    
+    if plot_data:
+        plt.figure(figsize=(6, 80))
+
+    Delta_uij_values = []     
+    sigmas = []
+       
+    for j in range(nlambdas-1):
+    
+        ##transitions from state 0 to 1 or 1 to 2, or 2 to 3 .... 
+        Ind_i = (thermo_states == j)
+        delta_u_ij = dhdl[Ind_i, j+1]       ##only for neighbored ensembles
+
+        #print ('lambda index=', j)
+        #print ('delta_u_ij.shape=', delta_u_ij.shape)
+      
+        #Delta_uij_values.append(delta_u_ij)
+
+        ### VAV debug
+        print('Are any delta_u_ij values nan?')
+        print(delta_u_ij)
+
+        mu, sigma = scipy.stats.norm.fit(delta_u_ij)
+        #print (mu, sigma)
+        sigmas.append(sigma)
+
+        delta_u_bins = np.arange(-15., 15., 0.2)
+        counts, bin_edges = np.histogram(delta_u_ij, bins=delta_u_bins)
+        counts = counts/counts.sum() # normalize
+        bin_centers = (bin_edges[0:-1] + bin_edges[1:])/2.0
+
+        if plot_data:
+            plt.subplot(nlambdas-1, 1, j+1)
+            plt.step(bin_centers, counts, label='$\Delta u_{%d \\rightarrow %d} \sigma$=%.2f'%(j,j+1,sigma))
+            #plt.xlabel('$\Delta u_{%d \\rightarrow %d}$'%(j, j+1))
+            plt.legend(loc='best')
+        
+    if plot_data:
+        plt.tight_layout()
+        plt.show()
+
+    ## VAV: hot fix for non-sampled lambda (sigma = nan), or sampled only once (sigma = 0)
+    max_sigma = max(sigmas)
+    for i in range(len(sigmas)):
+        if (sigmas[i] == 0) or np.isnan(sigmas[i]):
+            sigmas[i] = max_sigma
+
+
+    return np.array(sigmas)
+
+
+
+
---
+++ scripts/optimize_coul_vdw_lambdas.py
@@ -0,0 +1,332 @@
+import os, sys
+import numpy as np
+import matplotlib.pyplot as plt
+#  %matplotlib inline
+
+import scipy 
+from scipy import stats	
+
+from ee_tools import *
+
+
+def optimize_coul_vdw_lambdas(mdpfile, dhdl_xvgfile, outname, outdir, make_plots=True, save_plots=True, verbose=True):
+    """
+    DESCRIPTION
+        Optimize the lambda values for all intermediates
+        to minimize the total variance in P(\Delta u_ij) for neighboring thermodynamic ensembles
+
+    RETURNS
+        coul-lambdas, vdw-lambdas   (their optimized values)
+
+    WARNING
+        This script *assumes* that the coul and vdw lambda values
+        are changed sequentially, i.e. First the coul lambda goes from 0.0 to 1.0, then the vdw
+        lambda goes from 0.0 to 1.0, ideally with one of the intermediates having coul-lambda = 1.0, vdw-lambda = 0.0. 
+    """
+
+
+    coul_lambdas, vdw_lambdas = get_coul_vdw_lambdas(mdpfile)
+
+    # We map the two sets of values \in [0,1] to interval [0,2]
+    lambdas = coul_lambdas + vdw_lambdas
+    if verbose:
+        print('lambdas', lambdas)
+        print('lambdas.shape', lambdas.shape)
+
+    time_in_ps, thermo_states, dhdl, pV = get_dhdl_data(dhdl_xvgfile)
+    if verbose:
+        print('time_in_ps', time_in_ps)
+        print('thermo_states', thermo_states)
+        print('dhdl', dhdl)
+        print('pV', pV)
+
+    sigmas = estimate_sigmas(dhdl, thermo_states, plot_data=False)
+    if verbose:
+        print('sigmas.shape', sigmas.shape)
+ 
+    ### HOT FIX
+    ### If any of the sigma values are zero, then we know this is one of the cases where there are duplicate
+    ### fep-lambda
+
+    print('sigmas', sigmas)
+    indices_for_which_sigma_is_zero = np.where(sigmas < 0.0001)[0]
+    print('indices_for_which_sigma_is_zero', indices_for_which_sigma_is_zero)
+
+    # There should be only one such duplicate!
+    if len(indices_for_which_sigma_is_zero) == 1:
+        # For *THIS* particular data set we can see there is a problem:
+        # The i=49 --> i+1 = 50 lambdas are the same, resulting in a sigma of zero for that transition
+        # Let's remove it
+     
+        remove_index = indices_for_which_sigma_is_zero[0]
+        print('Removing DUPLICATE lambda index:', remove_index)
+        # print('sigmas', sigmas)
+        sigmas_list = sigmas.tolist()
+        sigmas_list.pop(remove_index)
+        sigmas = np.array(sigmas_list)
+        print('FIXED sigmas', sigmas)
+
+        print('lambdas',lambdas)
+        print('lambdas.shape',lambdas.shape)
+        lambdas_list = lambdas.tolist()
+        lambdas_list.pop(remove_index)
+        lambdas = np.array(lambdas_list)
+        print('FIXED lambdas',lambdas)
+        print('FIXED lambdas.shape',lambdas.shape)
+
+    elif len(indices_for_which_sigma_is_zero) > 1:
+        raise Exception("There are multiple zero sigma! Are there multiple duplicates in the lambda values?!")
+
+    else:
+        print('There are no duplicate lambda')
+        pass
+
+
+    ### Lambda optimization
+
+    dx = sigmas                 #according to Vince's equation (VAV: k is set to 1)
+    
+    x_values = np.cumsum(dx)    # convert to a list of x values of separated harmonic potentials
+    x_values = np.array(np.concatenate([[0], x_values]))    # add a zero corresponding to lambda0 = 0.0
+    ## VAV: This zero needs to be included.  Why was this left out before?
+    print('x_values', x_values)
+
+
+    from scipy.interpolate import UnivariateSpline
+    from scipy.interpolate import interp1d
+
+    if make_plots:
+        plt.figure(figsize=(12,6))
+
+    lambda_values = lambdas #not inclduing the first one, lambda_0 
+
+    x_observed = lambda_values      #not inclduing the first one, lambda_0
+    y_observed = x_values
+
+    if make_plots:
+        plt.subplot(1,2,1)
+        plt.plot(x_observed, y_observed, 'ro', label = 'data')
+        #plt.semilogy(x_observed, y_observed, 'ro', label = 'data')
+
+    #y_spl = CubicSpline(x_observed, y_observed)#, s=0,k=4)  
+    y_spl = UnivariateSpline(x_observed, y_observed, s=0, k=3)  
+    x_range = np.linspace(x_observed[0], x_observed[-1], 1000)
+
+
+    if make_plots:
+        plt.plot(x_range, y_spl(x_range), label="spline")   # for UnivariateSpline
+        ## plt.plot(x_observed, y_spl(x_observed), label="spline") # for CubicSpline
+        plt.legend()
+        plt.xlabel('lambda')
+        plt.ylabel('x values')
+
+        plt.subplot(1,2, 2)   #derivative plot
+
+    y_spl_1d = y_spl.derivative(n=1)    #n=1 , means the first order derivative
+    #print (y_spl_1d(x_observed))
+    # y_spl_1d = y_spl(x_observed, 1)  # first derivative of Cubic spline
+
+    if make_plots:
+        plt.plot(x_range, y_spl_1d(x_range), '-')
+        plt.plot(x_observed, y_spl_1d(x_observed), '.')
+        plt.ylabel('dx/dlambda')
+        
+        #plt.plot(x_observed, y_spl_1d, '.-', label='derivative')
+        plt.legend()
+        plt.xlabel('lambda')
+
+        if save_plots:
+            spline_pngfile = os.path.join(outdir, f'{outname}_splinefit.png') 
+            plt.savefig(spline_pngfile)
+            print(f'Wrote: {spline_pngfile}')
+
+
+
+    # Let's try a steepest descent algorithm like the kind I wrote up in "math-gradient-descent-2021-05-07.pdf" -VAV
+
+    # run the algorithm some fixed number of steps, or until some tolerance is reached
+    nsteps = 100000
+    tol = 1e-7  # stop if the lambdas dont change within this tolerance
+
+    alpha = 1e-5  # gradient descent step size
+    max_del_lambda = 0.0001   # the minimization step limited to this as a maximum change
+
+    print_every = 2000
+    
+    nlambdas = len(lambda_values)
+    print('lambda_values', lambda_values)
+    old_lambdas = np.array(lambda_values)
+    traj_lambdas = np.zeros( (nlambdas,nsteps) )
+    for step in range(nsteps):
+
+        # store the trajectory of lambdas
+        traj_lambdas[:,step] = old_lambdas
+        if verbose:
+            print('step', step, old_lambdas)
+    
+        # perform a steepest descent step
+        new_lambdas = np.zeros( old_lambdas.shape )
+        del_lambdas = np.zeros( old_lambdas.shape )
+        del_lambdas[0] = 0.0   # fix the \lambda = 0 endpoint
+        del_lambdas[nlambdas-1] = 0.0  # fix the \lambda = 1 endpoint
+    
+        if False:  # do in a loop (SLOW!) 
+            for i in range(1, (nlambdas-1)):
+                del_lambdas[i] = -1.0*alpha*2.0*y_spl_1d(old_lambdas[i])*( 2.0*y_spl(old_lambdas[i]) - y_spl(old_lambdas[i-1]) - y_spl(old_lambdas[i+1]))
+        else:   # do as a vector operation (FAST!) 
+            y_all = y_spl(old_lambdas)
+            yh, yi, yj = y_all[0:nlambdas-2], y_all[1:nlambdas-1], y_all[2:nlambdas] 
+            del_lambdas[1:nlambdas-1] = -1.0*alpha*2.0*y_spl_1d(old_lambdas[1:nlambdas-1])*( 2.0*yi - yh - yj)
+        if abs(np.max(del_lambdas)) > max_del_lambda:
+            del_lambdas[1:nlambdas-1] = del_lambdas[1:nlambdas-1]*max_del_lambda/np.max(del_lambdas)
+        new_lambdas = old_lambdas + del_lambdas
+        
+        # record the average change in the lambdas 
+        del_lambdas = np.abs(old_lambdas - new_lambdas).mean()
+        if step % print_every == 0:
+            print('step', step, 'del_lambdas', del_lambdas)
+        if del_lambdas < tol:
+            print('Tolerance has been reached: del_lambdas =', del_lambdas, '< tol =', tol)
+            break
+        
+        old_lambdas = new_lambdas
+   
+    if make_plots:     
+    
+        # Plot the results
+        plt.figure(figsize=(12,4))
+
+        plt.subplot(1,2,1)
+        for i in range(nlambdas):
+            plt.plot(range(step), traj_lambdas[i,0:step], '-')
+        plt.xlabel('step')
+        plt.ylabel('lambda values')
+        
+        plt.subplot(1,2,2)
+        for i in range(nlambdas):
+            plt.plot(range(step), y_spl(traj_lambdas[i,0:step]), '-')
+        plt.xlabel('step')
+        plt.ylabel('x values')
+
+        if save_plots:
+            traces_pngfile = os.path.join(outdir, f'{outname}_optimization_traces.png')
+            plt.savefig(traces_pngfile)
+            print(f'Wrote: {traces_pngfile}')
+
+
+    if make_plots:
+
+        plt.figure(figsize=(12,4))
+
+        plt.subplot(2,1,1)
+        plt.plot(x_range, y_spl(x_range), 'b-', label="spline")
+        plt.plot(lambda_values, y_spl(np.array(lambda_values)), 'r.', label="old lambdas")
+        for value in lambda_values:
+            plt.plot([value, value], [0, y_spl(value)], 'r-')
+        plt.legend()
+        plt.xlabel('lambda')
+        plt.ylabel('x values')
+        plt.title('old lambdas')
+
+        plt.subplot(2,1,2)
+        plt.plot(x_range, y_spl(x_range), 'b-', label="spline")
+        plt.plot(new_lambdas, y_spl(new_lambdas), 'g.', label="new lambdas")
+        for value in new_lambdas:
+            plt.plot([value, value], [0, y_spl(value)], 'g-')
+        plt.legend()
+        plt.xlabel('lambda')
+        plt.ylabel('x values')
+        plt.title('new lambdas')
+
+        if save_plots:
+            old_vs_new_lambdas_pngfile = os.path.join(outdir, f'{outname}_old_vs_new_lambdas.png')
+            plt.savefig(old_vs_new_lambdas_pngfile)
+            print(f'Wrote: {old_vs_new_lambdas_pngfile}')
+
+    # map the sum of coul-lambdas and vdw-lambdas back to their [0,1] ranges
+    new_coul_lambdas = np.minimum(new_lambdas, 1.0)
+    new_vdw_lambdas  = np.maximum(new_lambdas, 1.0) - 1.0
+
+    return new_coul_lambdas, new_vdw_lambdas
+
+
+########################
+
+if __name__ == '__main__':
+
+    usage  = """Usage:    python optimize_fep_lambdas.py mdpfile dhdl_xvgfile outname outdir
+
+    DESCRIPTION
+        This script will optimize the lambda values for all intermediates
+        to minimize the total variance in P(\Delta u_ij) for neighboring thermodynamic ensembles
+
+    OUTPUT
+        * A mdpfile-compatible string with new coul-lambdas and vdw-lambdas printed to std output
+        * Numpy arrays of the new values will be written to:
+            - [outdir]/[outname]_new_coul_lambdas.py
+            - [outdir]/[outname]_new_vdw_lambdas.py
+        * graphs associated with the lambda optimization will be written as images:
+            - [outdir]/[outname]_optimization_traces.png
+            - [outdir]/[outname]_old_vs_new_lambdas.png
+            - [outdir]/[outname]_splinefit.png
+        * a new expanded-ensemble mdp file: [outname]_ee_optimized.mdp
+              
+    EXAMPLE
+    Try this:
+        $ python ../scripts/optimize_coul_vdw_lambdas.py Alprenolol_tau0_0_water/ee.mdp Alprenolol_tau0_0_water/ee.dhdl.xvg opt Alprenolol_tau0_0_water
+    """        
+
+    # Parse input
+
+    if len(sys.argv) < 5:
+        print(usage)
+        sys.exit(1)
+
+    mdpfile      = sys.argv[1]
+    dhdl_xvgfile = sys.argv[2]
+    outname      = sys.argv[3]
+    outdir       = sys.argv[4]
+
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    assert os.path.exists(outdir)
+
+    # Optimize the lambdas
+    new_coul_lambdas, new_vdw_lambdas = optimize_coul_vdw_lambdas(mdpfile, dhdl_xvgfile, outname, outdir, make_plots=True, save_plots=True, verbose=False)
+
+
+    # print out new coul_lambdas, vdw_lambdas to std output, formatted like an mdp file
+    print('### Optimized lambda values ###')
+    print('')
+    ## coul_lambdas_string
+    coul_lambdas_string   = ' '.join(['%2.5f'%lam for lam in new_coul_lambdas])
+    print(f'coul-lambdas         = {coul_lambdas_string}')
+    ## vdw_lambdas_string
+    vdw_lambdas_string    = ' '.join(['%2.5f'%lam for lam in new_vdw_lambdas])
+    print(f'vdw-lambdas         = {vdw_lambdas_string}')
+    print()
+
+
+    new_coul_npyfile = os.path.join(outdir, f'{outname}_new_coul_lambdas.npy')
+    np.save(new_coul_npyfile, new_coul_lambdas)
+    print(f'Wrote: {new_coul_npyfile}')
+
+    new_vdw_npyfile = os.path.join(outdir, f'{outname}_new_vdw_lambdas.npy')
+    np.save(new_vdw_npyfile, new_vdw_lambdas)
+    print(f'Wrote: {new_vdw_npyfile}')
+
+    # write [outname]_ee_optimized.mdp to file, to run for 10 ns
+    from mdp_helpers import *
+    e = ExpandedPrep(ligand_only=True, couple_moltype = 'LIG',
+                     nsteps=5000000,  # 10000 ps = 10 ns simulation
+                     nstexpanded=250,  # 500 fs = 0.5 ps per swap and dhdl snapshot (20000 total energy snaps in the dhdl file)
+                     coul_lambdas=new_coul_lambdas, 
+                     vdw_lambdas=new_vdw_lambdas) 
+    ee_mdpfile = os.path.join(outdir, f'{outname}_ee_optimized.mdp')
+    e.write_to_filename(ee_mdpfile)
+    print(f'Wrote: {ee_mdpfile}')
+
+
+
+
+
